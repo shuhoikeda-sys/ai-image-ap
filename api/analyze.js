216d74cda9bb0a749663aebe9fb381ec891586c7
shuhoikeda-sys <shuho.ikeda@septeni.co.jp>
2026-03-30 21:27:17 +0900

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,11 +1,12 @@
 export default async function handler(req, res) {
   try {
+    // body安全取得（これ重要）
     const body = typeof req.body === "string"
       ? JSON.parse(req.body)
       : req.body;
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
@@ -18,6 +19,7 @@ export default async function handler(req, res) {
     const data = await response.json();
 
     res.status(200).json(data);
+
   } catch (err) {
     res.status(500).json({
       error: err.message,
